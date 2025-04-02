082f24a31cd61f9be40d31e6ab1adbea8372cee2
TasvirLimbani <tasvirlimbani111@gmail.com>
2025-04-02 15:20:33 +0530

    initial commit
    initial commit

--- lib/firebase/config.ts
+++ lib/firebase/config.ts
@@ -4,12 +4,14 @@ import { getFirestore } from "firebase/firestore"
 import { getStorage } from "firebase/storage"
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBjn1VybEnyLr6uJCyFGwfbSva3k1tOErM",
+  authDomain: "dtlive-22a03.firebaseapp.com",
+  databaseURL: "https://dtlive-22a03-default-rtdb.firebaseio.com",
+  projectId: "dtlive-22a03",
+  storageBucket: "dtlive-22a03.appspot.com",
+  messagingSenderId: "388888225316",
+  appId: "1:388888225316:web:f7cdfdb106cb7910689ab1",
+  measurementId: "G-4J3FCF7F32"
 }
 
 // Initialize Firebase
